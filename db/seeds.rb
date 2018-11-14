202411473f0021f608d8956745dcedddcf4e9363
john.kim <jkim21@pm.me>
2018-11-14 13:28:34 -0500

    Add test users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+john = User.create!(username: 'john', password: 'password')
+demo = User.create!(username: 'demo', password: 'password')
