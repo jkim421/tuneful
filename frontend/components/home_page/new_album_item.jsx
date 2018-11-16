1e5617dbc6d35dc09fd0132825cf2160f5c25ed5
john.kim <jkim21@pm.me>
2018-11-16 11:19:06 -0500

    Refactor new albums with new album items component

--- frontend/components/home_page/new_album_item.jsx
+++ frontend/components/home_page/new_album_item.jsx
@@ -4,7 +4,27 @@ import { Link } from 'react-router-dom';
 class NewAlbumItem extends React.Component {
 
   render() {
-    
+    const album = this.props.album
+    return (
+      <li className="new-album-item" key={album.id}>
+        <div className="new-album-show">
+          <div className="new-album-cover">
+            <Link className="new-album-link" to={`/album/${album.id}`} key={album.id} />
+          </div>
+          <div className="new-album-details" onMouseOver={this.displayCollect}>
+            <Link className="new-album-byline-link" to={`/album/${album.id}`}>
+              <p className="new-album-byline">{album.title}<br/>by {album.artist}</p>
+            </Link>
+            <p className="new-album-genre">{album.genre.toLowerCase()}</p>
+            <p className="new-album-description">{album.description}</p>
+            <button
+              className="new-album-collection uncollected">
+              add to collection
+            </button>
+          </div>
+        </div>
+      </li>
+    )
   }
 
 };
--- frontend/components/home_page/new_albums.jsx
+++ frontend/components/home_page/new_albums.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NewAlbumItemContainer from './new_album_item_container';
 
 class NewAlbums extends React.Component {
 
@@ -18,50 +19,13 @@ class NewAlbums extends React.Component {
   displayCollect(e) {
   }
 
-  showNewAlbums(albums) {
-    if (this.props.albums.length > 0) {
-      const albumDetails = albums.map( (album) => {
-        return (
-          <li className="new-album-item" key={album.id}>
-            <div className="new-album-show">
-              <div className="new-album-cover">
-                <Link className="new-album-link" to={`/album/${album.id}`} key={album.id} />
-              </div>
-              <div className="new-album-details" onMouseOver={this.displayCollect}>
-                <Link className="new-album-byline-link" to={`/album/${album.id}`}>
-                  <p className="new-album-byline">{album.title}<br/>by {album.artist}</p>
-                </Link>
-                <p className="new-album-genre">{album.genre.toLowerCase()}</p>
-                <p className="new-album-description">{album.description}</p>
-                <button
-                  className="new-album-collection uncollected">
-                  add to collection
-                </button>
-              </div>
-            </div>
-          </li>
-        )
-      })
-      return albumDetails;
-    } else {
-      return null
-    }
-  }
-
-
   render() {
-
-    let newAlbums;
-
-    if (this.props.albums.length > 0) {
-      newAlbums = this.props.albums;
-    }
-
+    const albums = this.props.albums.reverse();
     return (
       <section className="new-albums-container">
         <div className="home-separator-text">NEW AND NOTABLE</div>
           <ul className="new-albums-list">
-            {this.showNewAlbums(newAlbums)}
+            {albums.map( (album) => <NewAlbumItemContainer key={album.id} album={album}/>)}
           </ul>
       </section>
     );
