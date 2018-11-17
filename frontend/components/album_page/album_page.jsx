e99b96ab29aa634a66c786eb4bb16339b9f9c503
john.kim <jkim21@pm.me>
2018-11-16 20:56:49 -0500

    Create album page components

---
+++ frontend/components/album_page/album_page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AlbumPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+
+}
+
+export default AlbumPage;
---
+++ frontend/components/album_page/album_page_container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import AlbumPage from './album_page';
+
+const mapStateToProps = (state) => {
+  return {
+    <h1>ALBUM PAGE</h1>
+  };
+};
+
+export default connect(mapStateToProps)(AlbumPage);
