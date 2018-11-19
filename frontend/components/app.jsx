d6d4c595b3501cb3e46748deee2110f68d373b91
john.kim <jkim21@pm.me>
2018-11-19 11:08:04 -0500

    Edit footer margins

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util'
-import Modal from './modal';
+import Modal from './modal';ß
 import HeaderSelect from './header/header_select';
 import HomePageContainer from './home_page/home_page_container';
 import ArtistPageContainer from './artist_page/artist_page_container';
