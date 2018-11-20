9492ff89656a168b2c35ad893cce4f9d38babb4a
john.kim <jkim21@pm.me>
2018-11-20 18:50:30 -0500

    Add divs for player control

--- frontend/components/song_player/song_player.jsx
+++ frontend/components/song_player/song_player.jsx
@@ -6,12 +6,27 @@ class SongPlayer extends React.Component {
   }
 
   componentDidMount() {
+    const player = document.getElementById("player");
+  }
+
+  renderDuration() {
   }
 
   render() {
     return (
-      <div className="song-player">
-        SONGPLAYER
+      <div className="song-player-container">
+        <div className="song-player-controls">
+          <button className="play-btn">PP</button>
+          <div className="progress-bar-container">
+            <div className="player-title">Song Title</div>
+            <div className="progress-bar-bottom">
+              <div className="progress-bar"></div>
+              <div className="progress-bar-slider"></div>
+              <button className="prev-btn">-1</button>
+              <button className="next-btn">+1</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
