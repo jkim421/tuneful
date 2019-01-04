b36d0f594ab9b43293c5edbcf2328e7ee9c28c7f
john.kim <jkim21@pm.me>
2019-01-04 09:51:27 -0500

    Update search controller SQL query to order results by exact matches

--- app/controllers/api/search_results_controller.rb
+++ app/controllers/api/search_results_controller.rb
@@ -3,8 +3,12 @@ class Api::SearchResultsController < ApplicationController
   def index
     if !!params[:query]
       query = params[:query].downcase
-      @albums = Album.where("lower(title) LIKE ?", "%#{query}%").includes(:artist)
-      @artists = Artist.where("lower(name) LIKE ?", "%#{query}%")
+      @albums_ex = Album.where("lower(title) LIKE ?", "#{query}%").includes(:artist)
+      @albums_par = Album.where("lower(title) LIKE ? AND
+        lower(title) NOT LIKE ?", "%#{query}%", "#{query}%").includes(:artist)
+      @artists_ex = Artist.where("lower(name) LIKE ?", "#{query}%")
+      @artists_par = Artist.where("lower(name) LIKE ? AND
+        lower(name) NOT LIKE ?", "%#{query}%", "#{query}%")
       render "api/search_results/index.json.jbuilder"
     else
       render json: ["Invalid search"], status: 422
--- app/views/api/search_results/index.json.jbuilder
+++ app/views/api/search_results/index.json.jbuilder
@@ -1,12 +1,19 @@
 json.albums do
-  json.array! @albums.each do |album|
+  json.array! @albums_ex.each do |album|
+    json.extract! album, :id, :title, :release_date
+    json.artist album.artist.name
+  end
+  json.array! @albums_par.each do |album|
     json.extract! album, :id, :title, :release_date
     json.artist album.artist.name
   end
 end
 
 json.artists do
-  json.array! @artists.each do |artist|
+  json.array! @artists_ex.each do |artist|
+    json.extract! artist, :id, :name, :location
+  end
+  json.array! @artists_par.each do |artist|
     json.extract! artist, :id, :name, :location
   end
 end
